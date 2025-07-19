dd37b37cc51edb9c800a0984e9ad5c17be21a4a3
Vishwas-IITmandi <vishwaskureel@gmail.com>
2025-07-19 22:33:04 +0530

    directories created

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version = '0.0.0',
+    author = "Vishwas Kureel",
+    author_email= "vishwaskureel@gmail.com",
+    packages = find_packages(),
+    install_requires = []
+)
